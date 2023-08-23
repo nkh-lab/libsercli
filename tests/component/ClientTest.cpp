3a6ca4b43e32eb56eafdcbfc7b85854f2361c1c3
Mykola Khyliuk <nkh@ua.fm>
2023-08-23 11:15:36 +0200

    Add complex send command handling

--- tests/component/ClientTest.cpp
+++ tests/component/ClientTest.cpp
@@ -12,6 +12,7 @@
 #include <algorithm>
 #include <iostream>
 #include <sstream>
+#include <thread>
 
 #include "cpp-utils/StringHelper.h"
 #include "sercli/ClientBuilder.h"
@@ -19,12 +20,42 @@
 using namespace nkhlab::cpputils;
 using namespace nkhlab::sercli;
 
+void HandleSendCommand(IClient* client, const std::vector<uint8_t>& data, size_t nums, size_t delay_ms)
+{
+    std::cout << "Sending...\n";
+    for (size_t i = 0; i < nums; ++i)
+    {
+        if (!client->Send(data))
+        {
+            std::cout << "Failed!\n";
+            return;
+        }
+        if (delay_ms > 0 && i < nums - 1)
+            std::this_thread::sleep_for(std::chrono::milliseconds{delay_ms});
+    }
+    std::cout << "Done!\n";
+}
+
+void RetrieveNumsAndDelayFromSendCommand(const std::string& send_command, size_t& ret_nums, size_t& ret_delay_ms)
+{
+    ret_nums = 1;
+    ret_delay_ms = 0;
+
+    std::vector<std::string> sub_commands = StringHelper::SplitStr(send_command, ",");
+
+    for (auto s : sub_commands)
+    {
+        if (*s.begin() == 'n') ret_nums = std::stoi(s.substr(1));
+        if (*s.begin() == 'd') ret_delay_ms = std::stoi(s.substr(1));
+    }
+}
+
 //
 // Command format
 // q
 // q:
 // s:<data to send>
-// s,n10,t100ms:<data to send>
+// s,n<number of sending>,d<delay in ms, between sendings>:<data to send>
 //
 bool HandleCommand(const std::string& in, IClient* client)
 {
@@ -34,19 +65,22 @@ bool HandleCommand(const std::string& in, IClient* client)
 
     if (command_data.size() > 0)
     {
-        if (command_data[0] == "q")
-            ret_quit = true; // Quit
-        else
+        if (command_data[0] == "q") // Quit
         {
-            std::vector<std::string> commands = StringHelper::SplitStr(command_data[0], ",");
-
-            if (commands[0] == "s") // Send
+            ret_quit = true;
+        }
+        else if (command_data[0][0] == 's') // Send
+        {
+            if (command_data[1].size() > 0)
             {
-                if (commands.size() == 1)
-                {
-                    std::vector<uint8_t> data(command_data[1].begin(), command_data[1].end());
-                    client->Send(data);
-                }
+                std::vector<uint8_t> data(command_data[1].begin(), command_data[1].end());
+
+                size_t nums;
+                size_t delay_ms;
+
+                RetrieveNumsAndDelayFromSendCommand(command_data[0], nums, delay_ms);
+
+                HandleSendCommand(client, data, nums, delay_ms);
             }
         }
     }
@@ -77,7 +111,7 @@ int main(int argc, char const* argv[])
     }
     else
     {
-        std::cout << "Connection to Server FAILED!\n";
+        std::cout << "Connection to Server failed!\n";
     }
 
     return 0;
